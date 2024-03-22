8ba6619583968245080535f0eb22563c1e9fe505
anand <anandanand99599@gmail.com>
2024-03-22 21:33:48 +0530

    gocd server

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 component=$1
-ansible-playbook -i $component-dev.janand.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$component -e env=dev expense.yml
+ansible_playbook -i $component-dev.janand.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$component -e env=dev expense.yml
